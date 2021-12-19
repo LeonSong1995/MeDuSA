7306281470fcf639355e2a3b50177488308e87f1
LeonSong1995 <49676857+LeonSong1995@users.noreply.github.com>
2021-12-19 16:46:49 +0800

    Add files via upload

--- R/MeDuSA.R
+++ R/MeDuSA.R
@@ -2,30 +2,30 @@
 # Author: Liyang Song <songliyang@westlake.edu.cn>
 #############################################################################################################
 #' @title MeDuSA: mixed model-based deconvolution of cell-state abundance.
-#' @description \code{MeDuSA} is a fine-resolution cellular deconvolution method, with the aim to use reference scRNA-seq data to predict cell abundance distributed along a cell-state trajectory in a bulk RNA-seq data. MeDuSA is well suitable for biological scenarios in which the underlying mechanisms are associated with continuous transitions of cell-states.
+#' @description \code{MeDuSA} is a fine-resolution cellular deconvolution method, with the aim to use reference scRNA-seq data to predict the cell-state abundance of bulk RNA-seq data.
 #'
-#' @param bulk A matrix of bulk RNA-Seq data. Each row corresponds to a specific gene and each column corresponds to a particular sample.
-#' @param sce A \code{seurat} object of the single-cell RNA-Seq data (see \code{\link{seurat}}). Meta data of this seurat object need include 'cellType' and 'cellTrajectory'.
-#' @param selectCellType A character variable of the target cell type.
-#' @param ncpu The number of CPUs to be used.
-#' @param smooth A Boolean variable to determine whether to smooth the predicted cell-state abundance along the cell trajectory or not. The default value is TRUE. With default, the predicted cell-state abundance is smoothed using the Loess or averaging with its neighbors.
-#' @param smoothMethod A character vector of signature genes. The default value is NULL. With default, \code{MeDuSA} selects signature genes to distinguish cells within different cell-states using the generalized additive model (GAM, see \code{\link{mgcv}}).
-#' @param gene A character vector of signature genes. The default value is NULL. With default, \code{MeDuSA} selects signature genes to distinguish cells within different cell-states using the generalized additive model (GAM, see \code{\link{mgcv}}).
+#' @param bulk A matrix of bulk RNA-seq data. Each row corresponds to a specific gene and each column corresponds to a particular sample.
+#' @param sce A \code{seurat} object of the  reference scRNA-seq data (see \code{\link{seurat}}). Meta-data of the seurat object (sce@meta.data) need include two columns named as cell_type and cell_trajectory.
+#' @param select.ct A character variable of the target cell type.
+#' @param ncpu The number of CPU(s) to be used.
+#' @param smooth A Boolean variable to determine whether to smooth the predicted cell-state abundance or not. The default value is TRUE.
+#' @param smoothMethod A character variable to determine the smoothing method. The default method is \code{\link{loess}}.
+#' @param gene A character vector of signature genes. The default value is NULL. With default, \code{MeDuSA} selects signature genes using the generalized additive model (see \code{\link{mgcv}}).
 #' @param nbins A numeric variable to determine the number of bins along the cell trajectory, which is used to ensure the selected genes were uniformly scattered along the given trajectory. The default value is 10.
 #' @param resolution A numeric variable to determine the number of cell bins along the cell trajectory (i.e., the resolution of the deconvolution analysis). The default value is 50.
-#' @param knots A numeric variable to specify the numbers of knots of GAM. The default value is 10.
-#' @param start A vector for the initial value of the REML iteration. Default by c(1e-5,1e-2).
-#' @param maxgene A numeric variable to determine the maximum number of genes to be selected. The default value is 200.
+#' @param knots A numeric variable to specify the number of knots. The default value is 10.
+#' @param start A numeric vector for the initial value of the REML. Default by c(1e-5,1e-2).
+#' @param maxgene A numeric variable to determine the number of signature genes. The default value is 200.
 #' @param family A character variable to specify the distribution of GAM. See \code{\link{family.mgcv}} for a full list of what is available.
-#' @param gcov A matrix (or vector) of covariates in the GAM (i.e., covariates for selecting signature genes). The default value is NULL.
-#' @param Xc A matrix (or vector) of fixed covariates in the linear mixed model (i.e., covariates for predicting cell-state abundance). The default value is NULL.
-#' @param maxiter The maximum iteration numbers of AI-REML. The default value is 1e+4.
-#' @param adj A Boolean variable to determine whether to correct covariates in predicting cell-state abundance or not.
+#' @param gcov A matrix (vector) of covariates in the GAM. The default value is NULL.
+#' @param Xc A matrix (vector) of fixed covariates in the linear mixed model (i.e., covariates for predicting cell-state abundance). The default value is NULL.
+#' @param maxiter The iteration number of REML. Default by 1e+4.
+#' @param adj A Boolean variable to determine whether to include covariates when predicting the cell-state abundance.
 #'
 #' @return \code{MeDuSA} returns: \itemize{
-#' \item\code{abundance}: A matrix of cell-state abundance. Each row corresponds to a cell bin and each column corresponds to a sample.
-#' \item\code{gene}:A vector of the selected signature genes.
-#' \item\code{PesudoTimeCellbin}:A vector of the pseudo-time for each cell bin.
+#' \item\code{abundance}: A numeric matrix of cell-state abundance. Each row corresponds to a cell bin and each column corresponds to a sample.
+#' \item\code{gene}: A character vector of the selected signature genes.
+#' \item\code{PesudoTimeCellbin}: A numeric vector of the pseudo-time for each cell bin.
 #' }
 #'
 #' @author Liyang Song <songliyang@westlake.edu.cn>
@@ -41,38 +41,37 @@
 #'
 #' ##Build the 'Seurat' obejct:
 #' sce = CreateSeuratObject(ref)
-#' sce$cellType = cellType
-#' sce$cellTrajectory = rep(0,ncol(sce))
-#' sce$cellTrajectory[rownames(Trajectory)]=Trajectory
+#' sce$cell_type = cellType
+#' sce$cell_trajectory = rep(0,ncol(sce))
+#' sce$cell_trajectory[rownames(Trajectory)]=Trajectory
 #'
 #' ##Run MeDuSA (run with 6 courses):
-#' CellStateAbundance = MeDuSA(bulk=bulk,sce=sce,selectCellType='Epithelium',ncpu=6)
+#' csab = MeDuSA(bulk=bulk,sce=sce,select.ct='Epithelium',ncpu=6)
 
-MeDuSA = function(bulk,sce,selectCellType,ncpu=1,smooth=TRUE,smoothMethod='loess',gene=NULL,nbins=10,resolution=50,knots=10,start=c(1e-5,1e-2),maxgene=200,family='gaussian',gcov=NULL,Xc=NULL,maxiter=1e+4,adj=FALSE){
+MeDuSA = function(bulk,sce,select.ct,ncpu=1,smooth=TRUE,smoothMethod='loess',gene=NULL,nbins=10,resolution=50,knots=10,start=c(1e-5,1e-2),maxgene=200,family='gaussian',gcov=NULL,Xc=NULL,maxiter=1e+4,adj=FALSE){
 
 	#Checking the format of the input parameters
 	message("Thanks for using MeDuSA to perform cell-state abundance deconvolution analyses.")
 	if(!("Seurat" %in% class(sce))){
 	  stop('Please input Seurat Object.')
 	}
-	if(!'cellType' %in% colnames(sce@meta.data)){
-	  stop('Do you forget to input the cellType? Please check your MetaData: Seurat_Obj@meta.data.')
+	if(!'cell_type' %in% colnames(sce@meta.data)){
+	  stop('Do you forget to input the cell_type? Please check your MetaData: Seurat_Obj@meta.data.')
 	}
-	if(!'cellTrajectory' %in% colnames(sce@meta.data)){
-	  stop('Do you forget to input the cellTrajectory? Please check your MetaData: Seurat_Obj@meta.data.')
+	if(!'cell_trajectory' %in% colnames(sce@meta.data)){
+	  stop('Do you forget to input the cell_trajectory? Please check your MetaData: Seurat_Obj@meta.data.')
 	}
-	if(!is.null(selectCellType)){
-	  if(is.na(table(sce$cellType %in% selectCellType)['TRUE'])){
-	    stop('Do you forget to specify the cell type? Please check the selectCellType.')
+	if(!is.null(select.ct)){
+	  if(is.na(table(sce$cell_type %in% select.ct)['TRUE'])){
+	    stop('Do you forget to specify the cell type? Please check the select.ct.')
 	  }
 	}else{
-	  stop('Do you forget to specify the cell type? Please check the selectCellType.')
+	  stop('Do you forget to specify the cell type? Please check the select.ct.')
 	}
 
-
 	#Prepare the reference
-	index = which(sce$cellType==selectCellType)
-	space = as.matrix(sce$cellTrajectory[index])
+	index = which(sce$cell_type==select.ct)
+	space = as.matrix(sce$cell_trajectory[index])
 	space = as.matrix(space[order(space,decreasing = F),])
 	ref = as.matrix(sce@assays$RNA@counts[,rownames(space)])
 	commGene = intersect(rownames(bulk),rownames(sce))
